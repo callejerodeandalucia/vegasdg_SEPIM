cd9e48e5fce2fd60d6a104b4d830ae1f9b9434b7
fbma <fborja.manas@gmail.com>
2015-08-19 13:26:10 +0200

    Actualizados parámetros de configuración
    Urls de Geobúsquedas e id de app en SEPIM

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 url = "http://mapea-sigc.juntadeandalucia.es/sepim_server/api";
-urlCore = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_montefrio/search";
-urlSuggest = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_montefrio/suggest";
-idAplicacion = 33; //id de la aplicación
+urlCore = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_vegasdg/search";
+urlSuggest = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_vegasdg/suggest";
+idAplicacion = 41; //id de la aplicación
 aplicacion = null;
 pilaCategorias = [];
 coor_x = null;
