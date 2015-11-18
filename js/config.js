e5601ce328142e71debe642d82d276d23ebc32ef
Javier G. Leal <jgleal@gmail.com>
2015-11-18 14:23:00 +0100

    migración de SEPIM

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-url = "http://mapea-sigc.juntadeandalucia.es/sepim_server/api";
+url = "http://www.callejerodeandalucia.es/sepim/api";
 urlCore = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_vegasdg/search";
 urlSuggest = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sepim_vegasdg/suggest";
 idAplicacion = 41; //id de la aplicación
